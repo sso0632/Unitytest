c137c68a2d262e448cbe4aa810ea7c52447aff46
vpdrnls <gas0129@naver.com>
2018-01-18 01:28:47 +0900

    Test2
    TestB

--- Script/TEST2.cs
+++ Script/TEST2.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TEST2 : MonoBehaviour {
+public class TEST2 : MonoBehaviour
+{
 
+    int B;
 	// Use this for initialization
 	void Start () {
 		
