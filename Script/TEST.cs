b309bb5627e0d05e4185a2609242f0286ea4cc31
sso0632 <sso0632@naver.com>
2018-01-18 01:11:19 +0900

    FIRST
    F바보

---
+++ Script/TEST.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("장우철 바보");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
